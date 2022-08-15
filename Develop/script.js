9eb9c83a2cfccff7e074ed1597f718bab79c9757
cpivirotto <cpivirotto@mac.com>
2022-08-15 11:45:40 -0400

    new commit

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+var time = moment();
+var hour = new Date().getHours();
+document.querySelector("#currentDay").innerHTML = "Todays date: " + time.format("YYYY-DD-MM");
+// for (let i = 9; i < 17; i++) {
+//     console.log(document.querySelector(`#hour{i}`).getAttribute("data-hour"));
+//     if (time >= ())
+// }
+
+
---
+++ Develop/state current date and time.md
@@ -0,0 +1,5 @@
+state current date and time
+create objects for time with buttons
+    -if the time has changed the color of the bar will change
+    -upon load we need to retrive data from local storage
+when clicked it saves to local storage
